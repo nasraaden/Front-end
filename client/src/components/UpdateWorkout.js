68410d1639322b6a19c866a6946eef557087ffd5
Nasra <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2019-12-20 20:08:44 -0800

    app set up

---
+++ client/src/components/UpdateWorkout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UpdateWorkout = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default UpdateWorkout;
---
+++ client/src/components/WorkoutList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WorkoutList = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default WorkoutList;
