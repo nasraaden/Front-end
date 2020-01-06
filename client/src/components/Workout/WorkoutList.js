40c81e7d1b03c39da4bb2c7c886fc0b16557d8a4
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-01-06 16:19:35 -0700

    revert

--- client/src/components/Workout/WorkoutList.js
+++ client/src/components/Workout/WorkoutList.js
@@ -1,4 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+const workouts = [
+    {
+        id: 1,
+        name: "bench press",
+        weight: "185",
+        reps: "10",
+        date: "2019-12-31",
+        region: "chest"
+    },
+    {
+        id: 2,
+        name: "squat",
+        weight: "185",
+        reps: "10",
+        date: "2019-12-31",
+        region: "chest"
+    },
+    {
+        id: 3,
+        name: "bench press",
+        weight: "185",
+        reps: "10",
+        date: "2019-12-31",
+        region: "chest"
+    },
+]
+
+
 
 const WorkoutList = () => {
     return (
@@ -9,3 +39,5 @@ const WorkoutList = () => {
 }
 
 export default WorkoutList;
+
+// This is where you map over WorkoutCard to display the data that is being shown. 
