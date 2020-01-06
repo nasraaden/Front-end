4f4802a8dfa883e8b6c1dd403c0e777ac985d04f
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-01-06 12:59:49 -0700

    array of temp data created

--- client/src/components/WorkoutList.js
+++ client/src/components/WorkoutList.js
@@ -1,6 +1,55 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+const workouts = [
+    {
+        id: 1,
+        name: "bench press",
+        weight: "185",
+        reps: "10",
+        date: "2019-12-31",
+        region: "chest"
+    },
+    {
+        id: 2,
+        name: "squat",
+        weight: "185",
+        reps: "10",
+        date: "2019-12-31",
+        region: "chest"
+    },
+    {
+        id: 3,
+        name: "bench press",
+        weight: "185",
+        reps: "10",
+        date: "2019-12-31",
+        region: "chest"
+    },
+]
+
+
 
 const WorkoutList = () => {
+    const [workout, setWorkout] = useState([])
+    useEffect(() => {
+        const getWorkouts = () => {
+            // axios
+            //     .get('')
+            //     .then( res => {
+            //         setWorkout(res.)
+            //     })
+            //     .catch( err => {
+            //         console.log("unable to grab workouts", err)
+            //     })
+            setWorkout(workouts);
+        }
+
+        getWorkouts();
+    }, []);
+
+    console.log(workout);
+
     return (
         <div>
 
@@ -9,3 +58,5 @@ const WorkoutList = () => {
 }
 
 export default WorkoutList;
+
+// This is where you map over WorkoutCard to display the data that is being shown. 
