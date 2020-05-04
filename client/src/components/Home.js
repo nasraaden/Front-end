c7c238d710d3ea9a799d7036d3b3f293974b4a3d
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-05-04 07:28:42 -0700

    finished landing page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,74 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// images
+import mockup from '../images/mockup.png';
+import edit from '../images/edit.png';
+import calendar from '../images/calendar.png';
+import graph from '../images/graph.png';
+import floating from '../images/floating.png';
 
 const Home = () => {
-    return (
-        <>
-            <div className="home-container">
-                <h1>Welcome To Weightlifting Journal</h1>
-                <Link to="/signup"><button>Signup</button></Link>
-                <Link to="login"><button className="login">Login</button></Link>
-            </div>
-        </>
-    )
-}
+  return (
+    <div>
+      <div className='header'>
+        <img src={mockup} alt='iphone mockup of this app' />
+        <div className='header-right'>
+          <h3>Finally an app that tracks your lifts fast.</h3>
+          <p>
+            Focus on your workout with with WeightLifting Journal. Surpass your
+            limits with minimal distraction and clutter.
+          </p>
+          <button>Signup</button>
+        </div>
+      </div>
+      <div className='middle'>
+        <div className='section'>
+          <img src={edit} alt='edit icon' />
+          <div className='text'>
+            <h4>Create your own exercises</h4>
+            <p>
+              Weightlifting Journal gives you total control and customization
+              over your routines.
+            </p>
+          </div>
+        </div>
+        <div className='section'>
+          <img src={calendar} alt='calendar icon' />
+          <div className='text'>
+            <h4>Track your Progress</h4>
+            <p>
+              We give you the ability to track your exercises, and it's totally
+              up to you. Lift by lift, or set by set.
+            </p>
+          </div>
+        </div>
+        <div className='section'>
+          <img src={graph} alt='graph icon' />
+          <div className='text'>
+            <h4>Analyze your Consistency</h4>
+            <p>
+              View your workout patterns and analyze how to keep improving your
+              workouts!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='bottom'>
+        <div className='bottom-left'>
+          <h2>No distractions. Just Focus.</h2>
+          <p>
+            Weightlifting Journal offers one of the fastest ways to create, log
+            and track your workouts. Designed from the ground up to be a
+            distraction-free experience, the minimalist interface helps you to
+            focus on your workout.
+          </p>
+          <button>Try for Free</button>
+        </div>
+        <img src={floating} alt='floating screenshot of app' />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,38 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-
-    return (
-        <div className="nav-bar">
-            <div>
-                <h2>Weightlifting Journal</h2>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className='nav-bar'>
+      <div>
+        <h2>Weightlifting Journal</h2>
+      </div>
+      <div className='nav-links'>
+        <NavLink
+          exact
+          to='/'
+          className='nav-link'
+          activeClassName='nav-link-active'
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to='/signup'
+          className='nav-link'
+          activeClassName='nav-link-active'
+        >
+          Signup
+        </NavLink>
+        <NavLink
+          to='/login'
+          className='nav-link'
+          activeClassName='nav-link-active'
+        >
+          Login
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
